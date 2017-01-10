60121a2b664050d5f82169330556a676d7b42cdb
Brandon Steider <bsteider@cnm.edu>
2017-01-10 15:49:53 -0700

    added dank meme, and bad table chart

--- index.php
+++ index.php
@@ -5,6 +5,45 @@
 		<title>HTML SANDBOX</title>
 	</head>
 	<body>
+		<header>
+			<h1>this is a demo page!</h1>
+		</header>
+		<main>
+			<p>this is a demo page!</p>
+			<h3>this is a table</h3>
+			<table>
+				<!-- row 1 -->
+				<tr>
+					<th>colomn 1</th>
+					<th>column 2</th>
+				</tr>
+				<!-- row 2 -->
+				<tr>
+					<td>tabby</td>
+					<td>Beagle</td>
+				</tr>
+				<!-- row 3 -->
+				<tr>
+					<td>Persian</td>
+					<td>Bulldog</td>
+					<!-- row 4 -->
+				</tr>
+				<tr>Siamese</tr>
+				<tr>Boxer</tr>
+			</table>
 
+			<img src="images/doggo.jpg" alt="you thought i was doggo">
+
+			<p>this is a link to <a href="https://google.com">google</a></p>
+
+		</main>
+		<p>I think you've let your personal feelings cloud your judgement.</p>
+		<!-- this is a comment -->
+		<h2>A few of my favorite things</h2>
+		<ui>
+			<li>birds<li>
+			<li>snowboarding</li>
+			<li>Dank Kush</li>
+		</ui>
 	</body>
 </html>
